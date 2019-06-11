ce67c41ca75d423deec82dbd476649a5755572b2
cyberdragon42 <panchyshynmariait@gmail.com>
2019-06-11 18:02:33 +0300

    about page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import About from '../about';
 
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Header/>
-                <Body/>
+                <About/>
 
             </div>
         );
