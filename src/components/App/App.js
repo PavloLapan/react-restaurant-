da36b10f70e5cf406779bf404ee8f5ec6667c60f
LTania <tania.luchynska@gmail.com>
2019-06-12 16:04:53 +0300

    footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,26 +6,18 @@ import About from '../about';
 import '../../style/style.css'
 import '../../style/media.css'
 import Header from "../header";
-
-
-import '../../style/style.css';
-import '../../style/media.css';
 import Footer from "../footer";
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Footer/>
-
-
                 <Router>
                     <Switch>
                         <Header/>
                     </Switch>
                 </Router>
                 <About/>
-
-
+                <Footer/>
             </div>
         );
     }
