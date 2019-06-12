a1cc86e86f0ab44e2a2c7fbf25c965c950bfa562
Pavlo Lapan <pavlo042@gmail.com>
2019-06-12 15:19:30 +0300

    header
    header carcass

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,22 @@ import React, {Component} from 'react';
 
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
+import '../../style/style.css'
+import '../../style/media.css'
+import Header from "../header";
+
+
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Header/>
-                <Body/>
+                <Router>
+                    <Switch>
+                        <Header/>
+                    </Switch>
+                </Router>
+
 
             </div>
         );
